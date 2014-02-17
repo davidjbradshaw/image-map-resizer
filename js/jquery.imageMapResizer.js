726e93dd4a3b94fbf25fec2959c19c14fc3e435c
David J. Bradshaw <dave@bradshaw.net>
2014-02-17 17:15:21 +0100

    Code tidy up

--- js/jquery.imageMapResizer.js
+++ js/jquery.imageMapResizer.js
@@ -7,78 +7,82 @@
 
 (function($){
 
-    function setUpImageMap(){
-        function getCoords(){
-            for (var i = 0; i < len; i++) {
-                coords[i] = areas[i].coords.split(',');
-            }
-        }
+    //'use strict';
 
-        function getSourceImageWidthAndThenResizeMap(){
-            var
-                sourceImage = this,
-                testImage = new Image();
+    var defaults = {
+        debug: false
+    };
 
-            testImage.onload = function(){
-                imageWidth  = testImage.width;
-                imageHeight = testImage.height;
-                if ((sourceImage.width !== imageWidth) || (sourceImage.height !== imageHeight)){
-                    resizeMap();
+    $.fn.imageMapResize = function(options){
+        var settings = $.extend( {}, defaults, options );
+
+        return this.each(function(){
+            function getCoords(){
+                for (var i = 0; i < len; i++) {
+                    coords[i] = areas[i].coords.split(',');
                 }
-            };
+            }
 
-            testImage.src = sourceImage.src;
-        }
+            function getSourceImageWidthAndThenResizeMap(){
+                $mapImg.each(function(){
+                    var
+                        sourceImage = this,
+                        testImage = new Image();
 
-        function init(){
+                    testImage.onload = function(){
+                        imageWidth  = testImage.width;
+                        imageHeight = testImage.height;
+                        if ((sourceImage.width !== imageWidth) || (sourceImage.height !== imageHeight))
+                            resizeMap();
+                    };
+
+                    testImage.src = sourceImage.src;
+                });
+            }
+
+            function getCurrentImageWidth(){
+                return $mapImg.width();
+            }
+
+            function getCurrentImageHeight(){
+                return $mapImg.height();
+            }
+
+            function resizeMap() {
+                var
+                    i, j, clen,
+                    newCoords = [],
+                    sizeFactorWidth  = getCurrentImageWidth() / imageWidth;
+                    sizeFactorHeight = getCurrentImageHeight() / imageHeight;
+
+                for (i = 0; i < len; i++) {
+                    clen = coords[i].length;
+                    newCoords[i] = [];
+
+                    for (j = 0; j < clen; j+=2) {
+                        newCoords[i][j]   = parseInt(coords[i][j]   * sizeFactorWidth,  10);
+                        newCoords[i][j+1] = parseInt(coords[i][j+1] * sizeFactorHeight, 10);
+                    }
+
+                    areas[i].coords = newCoords[i].join(',');
+                    if (settings.debug) console.debug('['+areas[i].alt+'] '+ areas[i].coords);
+                }
+            }
+
+            var
+                map        = this,
+                $mapImg    = $('img[usemap=#'+$(map).attr('name')+']'),
+                areas      = map.getElementsByTagName('area'),
+                len        = areas.length,
+                coords     = [],
+                imageWidth,
+                imageHeight;
+            
             getCoords();
-            $mapImg.each(getSourceImageWidthAndThenResizeMap);
+            getSourceImageWidthAndThenResizeMap();
+            
             $(window).on('resize', resizeMap);
-        }
-
-        function getCurrentImageWidth(){
-            return $mapImg.width();
-        }
-
-        function getCurrentImageHeight(){
-            return $mapImg.height();
-        }
-
-        function resizeMap() {
-            var
-                i, j, clen,
-                newCoords = [],
-                sizeFactorWidth  = getCurrentImageWidth() / imageWidth;
-                sizeFactorHeight = getCurrentImageHeight() / imageHeight;
-
-            for (i = 0; i < len; i++) {
-                clen = coords[i].length;
-                newCoords[i] = [];
-
-                for (j = 0; j < clen; j+=2) {
-                    newCoords[i][j]   = parseInt(coords[i][j]   * sizeFactorWidth,  10);
-                    newCoords[i][j+1] = parseInt(coords[i][j+1] * sizeFactorHeight, 10);
-                }
-
-                areas[i].coords = newCoords[i].join(',');
-                console.debug('['+areas[i].alt+'] '+ areas[i].coords);
-            }
-        }
-
-        var
-            map        = this,
-            $mapImg    = $('img[usemap=#'+$(map).attr('name')+']'),
-            areas      = map.getElementsByTagName('area'),
-            len        = areas.length,
-            coords     = [],
-            imageWidth,
-            imageHeight;
-
-        init();
-    }
-
-    $.fn.imageMapResize = function(){
-        return this.each(setUpImageMap);
+        });
     };
 
 })(window.jQuery);

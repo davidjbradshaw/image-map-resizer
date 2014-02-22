81ee33020bcc057b72716a599f522e7d9d35da97
David J. Bradshaw <dave@bradshaw.net>
2014-02-22 15:40:31 +0100

    Simplify sourceImage into object

--- src/imageMapResizer.js
+++ src/imageMapResizer.js
@@ -11,38 +11,35 @@
         function resizeMap() {
             function resizeAreaTag(cachedAreaCoords){
                 function scaleCoord(e){
-                    return e * (1===(isWidth = 1-isWidth) ? scallingFactorWidth : scallingFactorHeight);
+                    return e * scallingFactor[(1===(isWidth = 1-isWidth) ? 'width' : 'height')];
                 }
 
                 var isWidth = 0;
 
-                return cachedAreaCoords.split(',').map(parseFloat).map(scaleCoord).join(',');
+                return cachedAreaCoords.split(',').map(parseFloat).map(scaleCoord).map(Math.floor).join(',');
             }
 
-            var
-                scallingFactorWidth  = displayedImage.width  / sourceImageWidth,
-                scallingFactorHeight = displayedImage.height / sourceImageHeight;
+            var scallingFactor = {
+                width  : displayedImage.width  / sourceImage.width,
+                height : displayedImage.height / sourceImage.height
+            };
 
             for (var i=0; i < areasLen ; i++)
                 areas[i].coords = resizeAreaTag(cachedAreaCoordsArray[i]);
         }
 
         function start(){
-            var testImage = new Image();
-            testImage.onload = function(){
-                sourceImageWidth  = testImage.width;
-                sourceImageHeight = testImage.height;
-
-                if ((displayedImage.width !== sourceImageWidth) || (displayedImage.height !== sourceImageHeight))
+            sourceImage.onload = function sourceImageOnLoadF(){
+                if ((displayedImage.width !== sourceImage.width) || (displayedImage.height !== sourceImage.height))
                     resizeMap();
             };
-            testImage.src = displayedImage.src;
+            sourceImage.src = displayedImage.src;
         }
 
         function listenForResize(){
             function debounce() {
                 clearTimeout(timer);
-                timer = setTimeout(resizeMap, 100);
+                timer = setTimeout(resizeMap, 250);
             }
             if (window.addEventListener) window.addEventListener('resize', debounce, false);
             else if (window.attachEvent) window.attachEvent('onresize', debounce);
@@ -55,9 +52,8 @@
             areasLen              = areas.length,
             cachedAreaCoordsArray = Array.prototype.map.call(areas,function (e) { return e.coords; }),
             displayedImage        = document.querySelector('img[usemap="#'+map.name+'"]'),
-            timer                 = null,
-            sourceImageWidth      = null,
-            sourceImageHeight     = null;
+            sourceImage           = new Image(),
+            timer                 = null;
         
         start();
         listenForResize();

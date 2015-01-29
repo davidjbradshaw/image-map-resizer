796563d7a00e795495dfce986ff534e6a5b8e328
David J. Bradshaw <dave@bradshaw.net>
2015-01-29 19:36:00 +0100

    Add support for NPM

--- gruntfile.js
+++ gruntfile.js
@@ -103,6 +103,9 @@ module.exports = function(grunt) {
 
           return retStr;
         }
+      },
+      npm:{
+        command: 'npm publish'
       }
     }
 
@@ -112,7 +115,7 @@ module.exports = function(grunt) {
   grunt.registerTask('notest',  ['jshint','uglify']);
   grunt.registerTask('test',    ['jshint','qunit']);
 
-  grunt.registerTask('postBump',['bump-commit','shell']);
+  grunt.registerTask('postBump',['git bump-commit','shell']);
   grunt.registerTask('patch',   ['default','bump-only:patch','postBump']);
   grunt.registerTask('minor',   ['default','bump-only:minor','postBump']);
   grunt.registerTask('major',   ['default','bump-only:major','postBump']);
